1f4b62623e54263f20e741fb206eff4bcd85607e
Lucas Samel <lucas.samel97@gmail.com>
2021-03-11 21:18:15 -0300

    Initial commit

---
+++ app/src/main/java/lucassamel/br/tp3_kotlinandroid/MainActivity.kt
@@ -0,0 +1,11 @@
+package lucassamel.br.tp3_kotlinandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
